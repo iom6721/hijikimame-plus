01e1eb73b3af2aedaa942f9d0436ea21b032ea89
iom <harusato620@gmail.com>
2026-08-17 05:59:11 +0000

    v2.3.5

--- hijikimame_desktop.py
+++ hijikimame_desktop.py
@@ -122,7 +122,7 @@ DEFAULT_EYE_COLOR = 'black'
 INVERTED_EYE_COLOR = 'white'
 
 # アプリバージョン（リリースタグと一致させてください）
-VERSION = "Snapshot-v2.3.5"
+VERSION = "v2.3.5"
 
 DISCORD_CLIENT_ID = "1507453857456721951"
 DISCORD_ACTIVITY_STATE = "※これは完全な身内ネタアプリケーションです。"
